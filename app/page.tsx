058ab7696ab37fd16fea91bf395aad19c74b46e4
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-06 16:08:40 +0900

    buttonLabelプロパティを削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import DataLoader from "@/features/component/DataLoader";
 
 export default function Home() {
-  return <DataLoader buttonLabel="ボタンを押す" />;
+  return <DataLoader />;
 }
--- features/component/DataLoader.tsx
+++ features/component/DataLoader.tsx
@@ -5,13 +5,9 @@ import Button from "@/components/Button";
 import LoadingPosts from "@/components/LoadingPosts";
 import { Suspense, useState } from "react";
 
-type TDataLoader = {
-  buttonLabel: string;
-};
-
 // 初期時はボタンのみを配置する。
 // ボタンがクリックされた場合にデータ取得処理を開始する。
-function DataLoader({ buttonLabel }: TDataLoader) {
+function DataLoader() {
   const [isButtonClick, setIsButtonClick] = useState<boolean>(false);
   const onButtonClick = async () => {
     setIsButtonClick(true);
@@ -19,7 +15,7 @@ function DataLoader({ buttonLabel }: TDataLoader) {
 
   return (
     <div className="m-10 h-full flex flex-col items-center justify-between space-y-10">
-      <Button onButtonClick={onButtonClick}>{buttonLabel}</Button>
+      <Button onButtonClick={onButtonClick}>データを取得する</Button>
       {isButtonClick && (
         <Suspense fallback={<Loading />}>
           <LoadingPosts />
