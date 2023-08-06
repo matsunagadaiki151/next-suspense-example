b2f12fca340e4a69cbcd44faf9ecf2ef85546543
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-08-06 16:38:04 +0900

    ロード画面を真ん中に配置

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,9 @@
 const Loading = () => {
   return (
-    <>
+    <div className="flex flex-col items-center">
       <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
       <div className="mt-4">ローディング中です。</div>
-    </>
+    </div>
   );
 };
 
